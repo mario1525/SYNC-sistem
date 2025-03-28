525176b0f66855a72804d8d35f8d3eea0356c06a
Mario Beltran <99507417+mario1525@users.noreply.github.com>
2025-03-28 14:17:37 -0500

    Objeto Guia

--- Backend/maintenace-service/src/Entity/Proced.cs
+++ Backend/maintenace-service/src/Entity/Proced.cs
@@ -6,8 +6,20 @@
         public string IdGuia { get; set; }          // FK de la tabla Guia
         public string Nombre { get; set; }          // Nombre del procedimiento
         public string Descripcion { get; set; }     // Descripcion del procedimiento
-        public bool Estado { get; set; }            // Estado
-        
+        public bool Estado { get; set; }            // Estado        
         public DateTime Fecha_log { get; set; }     // Log fecha
     }
+
+     public class ProcedC
+    {
+        public string Id { get; set; }              // Id interno del registro
+        public string IdGuia { get; set; }          // FK de la tabla Guia
+        public string Nombre { get; set; }          // Nombre del procedimiento
+        public string Descripcion { get; set; }     // Descripcion del procedimiento
+        public bool Estado { get; set; }            // Estado         
+        public DateTime Fecha_log { get; set; }     // Log fecha
+        public Valid[] valid { get; set; }
+    }
+
 }
+
